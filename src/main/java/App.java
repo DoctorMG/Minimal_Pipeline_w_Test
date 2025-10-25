ea2f71698021a5c93972efe1005813f50600a1ca
DoctorMG <drgellner@googlemail.com>
2025-10-25 17:05:52 +0200

    Test verfügbar gemacht

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,14 @@
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 7;
+        int b = 5;
+        int sum = add(a, b);
+
+        System.out.println("Mini-Rechner: a = " + a + " + b = " + b + " ergibt die Summe " + sum);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    void testAdd() {
+        assertEquals(5, App.add(2, 3), "Addition sollte korrekt sein");
+    }
+}
